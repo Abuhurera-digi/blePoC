afa7881208ba6f3547c53c1836756b1003d77a72
Monishka <monishka.ranjan@digitalsprint.ai>
2025-06-03 12:02:07 +0530

    fixed user queries

--- src/db/userQueries.js
+++ src/db/userQueries.js
@@ -1,23 +1,33 @@
 export const createUserTable = async (db) => {
-    const query = `CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      age INTEGER
-    );`;
-    await db.executeSql(query);
-  };
+  const query = `CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER,
+    device_id TEXT NOT NULL
+  );`;
+  await db.executeSql(query);
+};
   
-  export const insertUser = async (db, name, age) => {
-    const insertQuery = `INSERT INTO users (name, age) VALUES (?, ?);`;
-    await db.executeSql(insertQuery, [name, age]);
-  };
+export const insertUser = async (db, name, age, deviceId) => {
+  const insertQuery = `INSERT INTO users (name, age, device_id) VALUES (?, ?, ?);`;
+  await db.executeSql(insertQuery, [name, age, deviceId]);
+};
+
   
-  export const getUsers = async (db) => {
-    const results = await db.executeSql(`SELECT * FROM users;`);
-    return results[0].rows.raw();
-  };
+export const getUsers = async (db, deviceId) => {
+  console.log("Getting users for deviceId:", deviceId);
+  const results = await db.executeSql(
+    `SELECT * FROM users WHERE device_id = ?;`,
+    [deviceId]
+  );
+  const users = results[0].rows.raw();
+  console.log("Users fetched:", users);
+  return users;
+};
   
   export const deleteUser = async (db, id) => {
-    await db.executeSql(`DELETE FROM users WHERE id = ?;`, [id]);
+    const deleteQuery = `DELETE FROM users WHERE id = ?;`;
+    await db.executeSql(deleteQuery, [id]);
   };
+  
   
